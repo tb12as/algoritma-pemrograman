ca5798c215a11544a27e77a2d47d10aa9d68ee5f
Syafiq Afifuddin <syafiqafifuddin59@gmail.com>
2022-11-14 14:06:39 +0700

    fix: praktikum 3

--- 4 November 2022 - P6/percobaan_3_5.py
+++ 4 November 2022 - P6/percobaan_3_5.py
@@ -6,4 +6,10 @@ NumberOfCollegeGraduates = 2500
 number_of_college_graduates = 2500
 
 # mencetak variabel
-print(numberofcollegegraduates, NUMBEROFCOLLEGEGRADUATES, numberOfCollegeGraduates, NumberOfCollegeGraduates, number_of_college_graduates)
+print(
+	numberofcollegegraduates,
+	NUMBEROFCOLLEGEGRADUATES,
+	numberOfCollegeGraduates,
+	NumberOfCollegeGraduates,
+	number_of_college_graduates
+)
--- 4 November 2022 - P6/soal2.py
+++ 4 November 2022 - P6/soal2.py
@@ -2,7 +2,7 @@ nama = "Syafiq Afifuddin"
 tempat_lahir = "Jawai"
 tanggal_lahir = "12 Desember 2002"
 alamat = "Jalan Tanjung Raya 2, Gang Thamrin, nomor 4A"
-umur = 20
+umur = 19
 hobi = "bermain rubik dan membaca buku"
 
 print(f'Nama saya {nama}, tempat lahir saya {tempat_lahir}. saya lahir pada tanggal {tanggal_lahir}, alamat tempat tinggal saya di {alamat} di Kota Pontianak, umur saya sekarang {umur} tahun dan saya memiliki memiliki hobi {hobi}')
