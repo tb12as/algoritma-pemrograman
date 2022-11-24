c04fc3b2a0ff074084d260c6f9179dd44feb9822
Syafiq Afifuddin <syafiqafifuddin59@gmail.com>
2022-11-24 17:10:09 +0700

    get ready for uts 2

--- 25 November 2022 - P9/uts.py
+++ 25 November 2022 - P9/uts.py
@@ -8,12 +8,29 @@ def isInt(num):
 
 def intInput(message):
     value = input(message)
-    if(isInt(value)):
+    if (isInt(value)):
         return int(value)
     else:
         print('Input harus berupa angka!')
         return intInput(message)
 
 
-test = intInput('Masukkan angka : ')
-print(test)
+def menuInput():
+    index = intInput('Pilih menu : ') - 1
+    if (index in range(len(menu))):
+        return menu[index]
+    else:
+        print('Menu tidak ditemukan')
+        return menuInput()
+
+
+menu = [
+    {'name': 'Apaa', 'price': 5000},
+    {'name': 'Kubus', 'price': 51000}
+]
+
+for i in range(len(menu)):
+    print(f'{i+1}. {menu[i]["name"]} : {menu[i]["price"]}')
+
+product = menuInput()
+print(product)
