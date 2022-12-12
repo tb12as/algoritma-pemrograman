1a2ffa77ea5ceab77c47a7e98df045b0f42c3565
Syafiq Afifuddin <syafiqafifuddin59@gmail.com>
2022-12-13 03:53:42 +0700

    indent

--- 9 Desember 2022 - P11/modul-8/latihan/suit.py
+++ 9 Desember 2022 - P11/modul-8/latihan/suit.py
@@ -2,15 +2,17 @@ from random import randint
 
 
 jari = ['jempol', 'telunjuk', 'kelingking']
-computer = {'telunjuk' : ['jempol'], 'kelingking' : ['telunjuk'], 'jempol' : ['kelingking']}
+computer = {'telunjuk': ['jempol'], 'kelingking': [
+    'telunjuk'], 'jempol': ['kelingking']}
 
 print('Masukkan :q untuk keluar')
 while True:
     pemain = input(', '.join([v.title() for v in jari]) + '? ')
     pemain = pemain.lower()
     if pemain in jari:
-        komputer = [*computer[pemain], pemain][randint(0,1)] #https://stackoverflow.com/questions/68783326/unpack-list-of-list-python
-        #print('Menang atau seri :', [*computer[pemain], pemain])
+        # https://stackoverflow.com/questions/68783326/unpack-list-of-list-python
+        komputer = [*computer[pemain], pemain][randint(0, 1)]
+        # print('Menang atau seri :', [*computer[pemain], pemain])
         if pemain == komputer:
             print('Seri!')
         elif pemain == 'jempol':
@@ -32,6 +34,6 @@ while True:
         print(f'{"":5} {pemain} vs {komputer}\n')
 
     else:
-        if (pemain == ':q'): break
+        if (pemain == ':q'):
+            break
         print('Pilihan yang anda masukkan salah.')
-
