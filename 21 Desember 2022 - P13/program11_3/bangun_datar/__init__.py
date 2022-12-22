21b91b06613c768538a1f2c46f0ac8170a97aa2a
Syafiq Afifuddin <syafiqafifuddin59@gmail.com>
2022-12-22 22:07:43 +0700

    some fix & delete helper

--- 21 Desember 2022 - P13/program11_3/bangun_datar/__init__.py
+++ 21 Desember 2022 - P13/program11_3/bangun_datar/__init__.py
@@ -1,7 +1,7 @@
 # https://stackoverflow.com/questions/448271/what-is-init-py-for
 # https://www.programiz.com/python-programming/datetime/current-datetime
 
-from datetime import datetime
+# from datetime import datetime
 
-today = datetime.today()
-print(today.strftime("%d/%m/%Y %H:%M:%S"))
+# today = datetime.today()
+# print(today.strftime("%d/%m/%Y %H:%M:%S"))
