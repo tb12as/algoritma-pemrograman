e1369be2eb4a868c3a7333d41e6b86061e10ca39
Syafiq Afifuddin <syafiqafifuddin59@gmail.com>
2022-12-22 21:59:00 +0700

    rekursif

---
+++ 14 Desember 2022 - P12/howrekursifwork.py
@@ -0,0 +1,16 @@
+i = 1
+batas = 5
+
+if i < batas:
+    iDua = i + 1
+    if iDua < batas:
+        iTiga = iDua + 1
+        if iTiga < batas:
+            iEmpat = iTiga + 1
+            if iEmpat < batas:
+                iLima = iEmpat + 1
+                print(iLima)
+            print(iEmpat)
+        print(iTiga)
+    print(iDua)
+print(i)
